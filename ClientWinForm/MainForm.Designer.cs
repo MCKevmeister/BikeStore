238544f7098d7bf109e1f45396557582c3287e64
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-04-28 14:56:28 +1200

    Client Main form initial draft

---
+++ ClientWinForm/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace ClientWinForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categoryListView = new System.Windows.Forms.ListView();
+            this.categoryButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.manufacturerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // categoryListView
+            // 
+            this.categoryListView.HideSelection = false;
+            this.categoryListView.Location = new System.Drawing.Point(45, 58);
+            this.categoryListView.Name = "categoryListView";
+            this.categoryListView.Size = new System.Drawing.Size(306, 210);
+            this.categoryListView.TabIndex = 0;
+            this.categoryListView.UseCompatibleStateImageBehavior = false;
+            this.categoryListView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.categoryListView_MouseDoubleClick);
+            // 
+            // categoryButton
+            // 
+            this.categoryButton.Location = new System.Drawing.Point(45, 284);
+            this.categoryButton.Name = "categoryButton";
+            this.categoryButton.Size = new System.Drawing.Size(171, 43);
+            this.categoryButton.TabIndex = 1;
+            this.categoryButton.Text = "Open Category";
+            this.categoryButton.UseVisualStyleBackColor = true;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(45, 382);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(171, 43);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.AutoSize = true;
+            this.MainLabel.Location = new System.Drawing.Point(45, 22);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(125, 20);
+            this.MainLabel.TabIndex = 3;
+            this.MainLabel.Text = "Select a Category";
+            // 
+            // manufacturerButton
+            // 
+            this.manufacturerButton.Location = new System.Drawing.Point(45, 333);
+            this.manufacturerButton.Name = "manufacturerButton";
+            this.manufacturerButton.Size = new System.Drawing.Size(171, 43);
+            this.manufacturerButton.TabIndex = 1;
+            this.manufacturerButton.Text = "View Manufacturers";
+            this.manufacturerButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 447);
+            this.Controls.Add(this.MainLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.manufacturerButton);
+            this.Controls.Add(this.categoryButton);
+            this.Controls.Add(this.categoryListView);
+            this.Name = "MainForm";
+            this.Text = "Bike Store";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView categoryListView;
+        private System.Windows.Forms.Button categoryButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.Button manufacturerButton;
+    }
+}
+
---
+++ ClientWinForm/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientWinForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void updateCategoryListView()
+        {
+            categoryListView.Clear();
+            categoryListView.View = View.Details;
+            ListViewItem item1 = new("Mountian Bike");
+            ListViewItem item2 = new("Electric Bike");
+            categoryListView.Columns.Add("Categories", -2, HorizontalAlignment.Left);
+            categoryListView.Items.AddRange(new ListViewItem[] { item1, item2 });
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            updateCategoryListView();
+        }
+
+        private void categoryListView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
