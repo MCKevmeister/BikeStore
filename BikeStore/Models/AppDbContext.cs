8721a15e49532206e58bd014448f904a19ded88d
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-05-17 12:41:29 +1200

    Commiting before removing everything
    store of how crazy everything got trying to understand what i am doing

---
+++ BikeStore/Models/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BikeStoreServer.Models
+{
+    public class AppDbContext: DbContext
+    {
+        private readonly DbContextOptions _options;
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        public DbSet<Bike> Bikes { get; set; }
+
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     base.OnModelCreating(modelBuilder);
+        // }
+
+    }
+}
---
+++ BikeStore/Models/IBikeStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace BikeStoreServer.Models
+{
+    public interface IBikeStoreDatabaseSettings
+    {
+        public string OrderCollectionName { get; set; }
+        public string ManufacturerCollectionName { get; set; }
+        string BikeCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
