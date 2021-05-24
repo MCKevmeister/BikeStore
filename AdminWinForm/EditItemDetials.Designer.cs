c13381509f68ccfc7477e1f1462fdc7afb5f4975
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-05-24 18:11:44 +1200

    Administration win form skeleton

---
+++ AdminWinForm/EditItemDetials.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace AdminWinForm
+{
+    partial class EditItemDetials
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditItemDetials));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.addImageButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Item Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(586, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Merida eSpresso 500 EQ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Item Description:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(156, 100);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(586, 163);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(248, 2);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(125, 27);
+            this.maskedTextBox1.TabIndex = 4;
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainLabel.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.MainLabel.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.MainLabel.Location = new System.Drawing.Point(0, 0);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(800, 55);
+            this.MainLabel.TabIndex = 5;
+            this.MainLabel.Text = "Edit Item Detials";
+            this.MainLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 269);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 27);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "$4900";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Price:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(156, 302);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 27);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Stock:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Manufacturer:";
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(156, 336);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(150, 27);
+            this.domainUpDown1.TabIndex = 12;
+            this.domainUpDown1.Text = "Merida";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(96, 374);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Motor:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(156, 371);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(256, 27);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.Text = "Shimano E6100 60Nm";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(28, 523);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(171, 42);
+            this.exitButton.TabIndex = 15;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(28, 473);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(171, 42);
+            this.saveButton.TabIndex = 16;
+            this.saveButton.Text = "Save and Exit";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // addImageButton
+            // 
+            this.addImageButton.Location = new System.Drawing.Point(28, 427);
+            this.addImageButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.addImageButton.Name = "addImageButton";
+            this.addImageButton.Size = new System.Drawing.Size(171, 42);
+            this.addImageButton.TabIndex = 17;
+            this.addImageButton.Text = "Add Image";
+            this.addImageButton.UseVisualStyleBackColor = true;
+            // 
+            // EditItemDetials
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 574);
+            this.Controls.Add(this.addImageButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MainLabel);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "EditItemDetials";
+            this.Text = "NewItemForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button addImageButton;
+    }
+}
---
+++ AdminWinForm/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace AdminWinForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.viewItemButton = new System.Windows.Forms.Button();
+            this.itemListView = new System.Windows.Forms.ListView();
+            this.viewOrdersButton = new System.Windows.Forms.Button();
+            this.addItemButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(25, 390);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(171, 43);
+            this.closeButton.TabIndex = 6;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // viewItemButton
+            // 
+            this.viewItemButton.Location = new System.Drawing.Point(25, 292);
+            this.viewItemButton.Name = "viewItemButton";
+            this.viewItemButton.Size = new System.Drawing.Size(171, 43);
+            this.viewItemButton.TabIndex = 8;
+            this.viewItemButton.Text = "View Item";
+            this.viewItemButton.UseVisualStyleBackColor = true;
+            this.viewItemButton.Click += new System.EventHandler(this.categoryButton_Click);
+            // 
+            // itemListView
+            // 
+            this.itemListView.HideSelection = false;
+            this.itemListView.Location = new System.Drawing.Point(12, 76);
+            this.itemListView.Name = "itemListView";
+            this.itemListView.Size = new System.Drawing.Size(730, 210);
+            this.itemListView.TabIndex = 5;
+            this.itemListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // viewOrdersButton
+            // 
+            this.viewOrdersButton.Location = new System.Drawing.Point(202, 292);
+            this.viewOrdersButton.Name = "viewOrdersButton";
+            this.viewOrdersButton.Size = new System.Drawing.Size(171, 43);
+            this.viewOrdersButton.TabIndex = 8;
+            this.viewOrdersButton.Text = "View Orders";
+            this.viewOrdersButton.UseVisualStyleBackColor = true;
+            this.viewOrdersButton.Click += new System.EventHandler(this.viewOrdersButton_Click);
+            // 
+            // addItemButton
+            // 
+            this.addItemButton.Location = new System.Drawing.Point(25, 341);
+            this.addItemButton.Name = "addItemButton";
+            this.addItemButton.Size = new System.Drawing.Size(171, 43);
+            this.addItemButton.TabIndex = 11;
+            this.addItemButton.Text = "Add Item";
+            this.addItemButton.UseVisualStyleBackColor = true;
+            this.addItemButton.Click += new System.EventHandler(this.addItemButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 62);
+            this.label1.TabIndex = 10;
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainLabel.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.MainLabel.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.MainLabel.Location = new System.Drawing.Point(0, 0);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(800, 73);
+            this.MainLabel.TabIndex = 9;
+            this.MainLabel.Text = "Current Stock";
+            this.MainLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.addItemButton);
+            this.Controls.Add(this.MainLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.viewOrdersButton);
+            this.Controls.Add(this.viewItemButton);
+            this.Controls.Add(this.itemListView);
+            this.Name = "MainForm";
+            this.Text = "Bike Store Admin";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button viewItemButton;
+        private System.Windows.Forms.ListView itemListView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button addItemButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.Button viewOrdersButton;
+    }
+}
+
