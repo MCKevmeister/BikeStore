66fcef67dd4f3cc348de92b1b8c81de1253f1130
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-06-02 11:44:48 +1200

    Update IRepository.cs

--- BikeStoreApi/Repositories/IRepository.cs
+++ BikeStoreApi/Repositories/IRepository.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace BikeStoreApi.Repositories
 {
-    public interface IRepository<TEntity> where TEntity : class
+    public interface IRepository<TEntity>: IDisposable where TEntity : class
     {
-
-
-        public void Create(TEntity obj);
-        public void Update(TEntity obj);
-        public void Delete(string id);
-        public Task<TEntity> GetById(string id);
-        public Task<IEnumerable<TEntity>> GetAll();
+        Task Create(TEntity obj);
+        Task Update(TEntity obj);
+        Task Delete(string id);
+        Task<TEntity> GetById(string id);
+        Task<IEnumerable<TEntity>> GetAll();
     }
 }
+//https://bryanavery.co.uk/asp-net-core-mongodb-repository-pattern-unit-of-work/ 2 June 21
