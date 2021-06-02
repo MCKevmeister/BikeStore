6d4a0e76e0361ca97d389dc1887d2348338f4613
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-06-02 16:37:49 +1200

    Create ManufacturerResponse.cs

---
+++ Models/Responses/ManufacturerResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace BikeStore.Models.Responses
+{
+    public class ManufacturerResponse
+    {
+        public ManufacturerResponse(Manufacturer manufacturer)
+        {
+            if (manufacturer == null) return;
+            Manufacturer = manufacturer;
+        }
+        
+        [JsonProperty("movie", NullValueHandling = NullValueHandling.Ignore)]
+        public Manufacturer Manufacturer { get; set; }
+    }
+}
