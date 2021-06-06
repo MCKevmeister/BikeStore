e4296ee7b46b5bacfb7ef48401d8bd204fee36c9
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-06-06 21:22:21 +1200

    Update OrderRepository.cs

--- BikeStoreApi/Repositories/OrderRepository.cs
+++ BikeStoreApi/Repositories/OrderRepository.cs
@@ -1,34 +1,17 @@
-﻿using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using BikeStore.Models;
-
-namespace BikeStoreApi.Repositories
+﻿namespace BikeStoreApi.Repositories
 {
-    public class OrderRepository : Repository<Order>, IOrderRepository
+    public class OrderRepository : IOrderRepository
     {
-        public OrderRepository(IMongoContext context) : base(context)
+        private readonly IMongoContext _context;
+
+        public OrderRepository(IMongoContext context)
         {
+            _context = context;
         }
 
-        public Task<Order> GetByName(string name)
+        public void Dispose()
         {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<IEnumerable<Order>> GetAll()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<Order> Create(string manufacturerName, CancellationToken cancellationToken)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task Delete(Order manufacturer)
-        {
-            throw new System.NotImplementedException();
+            _context?.Dispose();
         }
     }
 }
