d98af7f1f876d0a02594acb38078030ad377b3e4
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-06-06 15:41:44 +1200

    Update UnitOfWork.cs

--- BikeStoreApi/UnitOfWork.cs
+++ BikeStoreApi/UnitOfWork.cs
@@ -11,7 +11,7 @@ namespace BikeStoreApi
         private readonly IMongoContext _context;
         private IManufacturerRepository _manufacturerRepository;
         private IOrderRepository _orderRepository;
-        private bool _disposed;
+        //IBikeRepository BikeRepository { get; }
 
         public UnitOfWork(IMongoContext context)
         {
@@ -22,34 +22,12 @@ namespace BikeStoreApi
         public IOrderRepository OrderRepository => _orderRepository ??= new OrderRepository(_context);
         public async Task<bool> Commit()
         {
-            var changeAmount = await _context.SaveChanges();
+            var changeAmount = await _context.CommitChanges();
             return changeAmount > 0;
-        }
-
-        public Task<IClientSessionHandle> StartSessionAsync(ClientSessionOptions options = null,
-            CancellationToken cancellationToken = default(CancellationToken))
-        {
-            throw new System.NotImplementedException();
-        }
-        
-
-        public Task CommitTransactionAsync(CancellationToken cancellationToken = default(CancellationToken))
-        {
-            throw new System.NotImplementedException();
-        }
-        
-        private void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                if (disposing) { _context.Dispose(); }
-            }
-            _disposed = true;
         }
 
         public void Dispose()
         {
-            Dispose(true);
             GC.SuppressFinalize(this);
         }
     }
