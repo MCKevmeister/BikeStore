28196c3b971fe543e8ecda59c31684cb80e36255
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-06-10 07:40:25 +1200

    Update RepositoriesExtension.cs

--- BikeStoreApi/Repositories/RepositoriesExtension.cs
+++ BikeStoreApi/Repositories/RepositoriesExtension.cs
@@ -1,36 +1,66 @@
 ﻿using BikeStore.Models;
-using BikeStoreApi.Services;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.Serializers;
 
 namespace BikeStoreApi.Repositories
 {
    public static class RepositoryExtensions
     {
+        public static void Configure()
+        {
+            BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.CSharpLegacy));
+
+            var pack = new ConventionPack
+            {
+                new CamelCaseElementNameConvention(),
+                new IgnoreExtraElementsConvention(true),
+                new IgnoreIfDefaultConvention(true)
+            };
+            ConventionRegistry.Register("My Solution Conventions", pack, t => true);
+        }
         public static void RegisterMongoDbRepositories(this IServiceCollection servicesBuilder)
         {
             servicesBuilder.AddSingleton<IManufacturerRepository, ManufacturerRepository>();
-            //servicesBuilder.AddSingleton<IBikeRepository, BikeRepository>();
-            //servicesBuilder.AddSingleton<IOrderRepository, OrderRepository>();
-        }
-
-        public static void RegisterMongoDbServices(this IServiceCollection servicesBuilder)
-        {
-            servicesBuilder.AddSingleton<IManufacturerService, ManufacturerService>();
-            //servicesBuilder.AddSingleton<IBikeService, BikeService>();
-            //servicesBuilder.AddSingleton<IOrderService, OrderService>();
+            servicesBuilder.AddSingleton<IBikeRepository, BikeRepository>();
+            servicesBuilder.AddSingleton<IOrderRepository, OrderRepository>();
         }
 
         public static void RegisterMongoDbClassMaps(this IServiceCollection servicesBuilder)
         {
-            /*BsonClassMap.RegisterClassMap<Bike>(cm => {
-                cm.AutoMap();
-                cm.SetIsRootClass(true);
+            /*BsonClassMap.RegisterClassMap<Bike>(map => {
+                map.AutoMap();
+                map.SetIsRootClass(true);
+                map.SetIgnoreExtraElements(true);
+                map.MapIdMember(x => x.Id);
             });
-            BsonClassMap.RegisterClassMap<ElectricBike>();
-            BsonClassMap.RegisterClassMap<RoadBike>();*/
-            BsonClassMap.RegisterClassMap<Order>();
-            BsonClassMap.RegisterClassMap<Manufacturer>();
+            BsonClassMap.RegisterClassMap<ElectricBike>(map => {
+                map.AutoMap();
+                map.SetIgnoreExtraElements(true);
+                map.MapIdMember(x => x.Id);
+            });
+            BsonClassMap.RegisterClassMap<RoadBike>(map => {
+                map.AutoMap();
+                map.SetIsRootClass(true);
+                map.SetIgnoreExtraElements(true);
+                map.MapIdMember(x => x.Id);
+            });
+            BsonClassMap.RegisterClassMap<Order>(map => {
+                map.AutoMap();
+                map.SetIsRootClass(true);
+                map.SetIgnoreExtraElements(true);
+                map.MapIdMember(x => x.Id);
+            });*/
+            BsonClassMap.RegisterClassMap<Manufacturer>(map => {
+                map.AutoMap();
+                map.SetIsRootClass(true);
+                map.SetIgnoreExtraElements(true);
+                map.MapIdMember(manufacturer => manufacturer.Id);
+                map.IdMemberMap.SetRepresentation(BsonType.ObjectId);
+                map.MapMember(manufacturer => manufacturer.Name).SetIsRequired(true);
+            });
         }
     }
 }
