3ee4ddb8cfb70702c2ad7a06b7b975ba0579b4b1
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-06-10 21:06:24 +1200

    Create OrderRepository.cs

---
+++ BikeStoreApi/Controllers/OrderRepository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BikeStore.Models;
+using BikeStoreApi.Repositories;
+using BikeStoreApi.UnitOfWork;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BikeStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork, IOrderRepository orderRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _orderRepository = orderRepository;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetAll()
+        {
+            var orders = await _orderRepository.GetAll();
+            return Ok(orders);
+        }
+        
+        [HttpGet("{id}", Name = "GetOrderAsync")]
+        public async Task<ActionResult<Order>> Get(string id)
+        {
+            var order = await _orderRepository.GetById(id);
+            if (order == null)
+            {
+                return BadRequest();
+            }
+            return Ok(order);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<Order>> Create(Order order)
+        {
+            _orderRepository.Create(order);
+            await _unitOfWork.Commit();
+            var newOrder = await _orderRepository.GetById(order.Id);
+            if (newOrder == null)
+            {
+                return BadRequest();
+            }
+            return Ok(newOrder);
+        }
+        
+        [HttpPut]
+        public async Task<ActionResult<Order>> UpdateOrderAsync(Order order)
+        {
+            var oldOrderCheck = await _orderRepository.GetById(order.Id);
+            if (oldOrderCheck == null)
+                return NotFound();
+            _orderRepository.Update(order, order.Id);
+            await _unitOfWork.Commit();
+            return Ok(order);
+        }
+        
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var orderToDelete = await _orderRepository.GetById(id);
+            if (orderToDelete == null)
+                return BadRequest();
+            _orderRepository.Delete(id);
+            await _unitOfWork.Commit();
+            var deletedOrder = await _orderRepository.GetById(id);
+            if (deletedOrder == null)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }   
+}
