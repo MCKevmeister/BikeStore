a201a3eba329dd3a860f57b4e87f2fd6b2678f7c
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-06-22 13:17:34 +1200

    Update RoadBike.cs

--- Models/RoadBike.cs
+++ Models/RoadBike.cs
@@ -7,11 +7,13 @@ namespace BikeStore.Models
     public class RoadBike : Bike
     {
         public RoadBike(string name, string manufacturerId, decimal price, string description, int weight,
-            DateTime lastModified, int stockAmount, string breakType) : base( name,  manufacturerId,  price,  description,  weight,
-            lastModified,  stockAmount)
+            DateTime lastModified, int stockAmount, string breakType) : base(name, manufacturerId, price, description, 
+            weight, lastModified, stockAmount)
         {
             BrakeType = breakType;
         }
-        [BsonElement("brakeType")] [Required] public string BrakeType { get; set; }
+        [BsonElement("brakeType")] 
+        [Required] 
+        public string BrakeType { get; set; }
     }
 }
